d75faea445ae90d7d156ff94f070bbc497c7aed4
Akhil-akku <136787384+Akhil-akku@users.noreply.github.com>
2024-09-02 15:54:23 +0530

    Login & Signup pages UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import UserModal from '../components/UserModal'
+import SignUp from '../components/SignUp'
+
 // import Navbar
-import Avatar from '@mui/material/Avatar';
 const Home = () => {
   return (
-    // <Navbar/>
-    <Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1721332150382-d4114ee27eff?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    <div>
+
+
+     {/* <Navbar/> */}
+    <UserModal/>
+    {/* <SignUp/> */}
+       
+    
+    
+    
+    </div>
   )
 }
 
